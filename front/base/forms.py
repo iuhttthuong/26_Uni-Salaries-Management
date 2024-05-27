0ed18a9bca92f3903adf42395d22639351e6a63e
iuhttthuong <111831564+iuhttthuong@users.noreply.github.com>
2024-05-27 22:59:17 +0700

    Delete front directory

--- front/base/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(label="Mã giảng viên", widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label="Mật khẩu", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    
